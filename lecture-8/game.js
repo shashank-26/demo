fc83115a200190f973cb00d66157062363ed77cb
shashank <shashankchaurasia97@gmail.com>
2018-07-08 15:18:13 +0530

    lecture added

---
+++ lecture-8/game.js
@@ -0,0 +1,11 @@
+function game(name,player){
+  this.name=name;
+  this.palyer=player;
+}
+
+game.prototye.getGame=function(){
+  console.log(this.name,this.player);
+}
+game.prototype.updategame=function(gam){
+  this.namegam
+}
---
+++ lecture-8/todo.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+  let button=document.getElementsByid('btn');
+  let input= document.getElementById('inp');
+  let output=document.getElementById('result');
+
+  button.onclick=function () {
+    value+=`<li>`
+  }
+}
